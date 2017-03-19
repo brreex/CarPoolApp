6e9c48cad7faf4c7d0dfb659f3f838cfe612e4c5
biniam1990 <biniamw1990@gmail.com>
2017-03-19 00:27:03 -0500

    new changes

--- CarPoolApp/src/com/dataaccess/dataaccessfacade.java
+++ CarPoolApp/src/com/dataaccess/dataaccessfacade.java
@@ -3,11 +3,10 @@ package com.dataaccess;
 import java.time.LocalDate;
 import java.util.Date;
 
-
-
 public interface dataaccessfacade {
 	public boolean login(String username, String password);
-	public static void registerUser(String fname,String lname,
-			String gender, int bdate, String email, String city, 
-			String state, String street, int zipcode){};
+
+	public static void registerUser(String fname, String lname, String gender, int bdate, String email, String city,
+			String state, String street, int zipcode) {
+	};
 }
--- CarPoolApp/src/com/model/User.java
+++ CarPoolApp/src/com/model/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 import com.dataaccess.DataAcess;
 
 public class User {
-	
+
 	private String firstName;
 	private String lastName;
 	private String gender;
@@ -26,13 +26,13 @@ public class User {
 	private List<Comment> comment;
 	private List<Like> like;
 
-	public User(){
-		
+	public User() {
+
 	}
-	public User(String firstName, String lastName, String gender, int birthDate, 
-			String email, String city, String state, 
-			String street, int zipcode,String password,String username) {
-		
+
+	public User(String firstName, String lastName, String gender, int birthDate, String email, String city,
+			String state, String street, int zipcode, String password, String username) {
+
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.gender = gender;
@@ -44,208 +44,138 @@ public class User {
 		this.state = state;
 		this.street = street;
 		this.zipcode = zipcode;
-		this.password=password;
-		this.username=username;
+		this.password = password;
+		this.username = username;
 	}
-	
-	
-	
 
 	public String getFirstName() {
 		return firstName;
 	}
 
-
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
-
 
 	public String getLastName() {
 		return lastName;
 	}
 
-
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
-	
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
-
 
 	public String getUsername() {
 		return username;
 	}
 
-
-
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
-
-
 
 	public String getGender() {
 		return gender;
 	}
 
-
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
-
 
 	public int getBirthDate() {
 		return birthDate;
 	}
 
-
-
 	public void setBirthDate(int birthDate) {
 		this.birthDate = birthDate;
 	}
-
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
 
 	public Date getDateUpdated() {
 		return dateUpdated;
 	}
 
-
-
 	public void setDateUpdated(Date dateUpdated) {
 		this.dateUpdated = dateUpdated;
 	}
-
-
 
 	public Date getDateCreated() {
 		return dateCreated;
 	}
 
-
-
 	public void setDateCreated(Date dateCreated) {
 		this.dateCreated = dateCreated;
 	}
-
-
 
 	public String getCity() {
 		return city;
 	}
 
-
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
-
 
 	public String getState() {
 		return state;
 	}
 
-
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
-
 
 	public String getStreet() {
 		return street;
 	}
 
-
-
 	public void setStreet(String street) {
 		this.street = street;
 	}
-
-
 
 	public int getZipcode() {
 		return zipcode;
 	}
 
-
-
 	public void setZipcode(int zipcode) {
 		this.zipcode = zipcode;
 	}
-
-
 
 	public List<Post> getPost() {
 		return post;
 	}
 
-
-
 	public void addPost(Post post) {
-		this.post= new ArrayList<Post>();
-		  this.post.add(post);
+		this.post = new ArrayList<Post>();
+		this.post.add(post);
 	}
-
-
 
 	public List<Comment> getComment() {
 		return comment;
 	}
-
-
 
 	public void addComment(Comment comment) {
 		this.comment = new ArrayList<Comment>();
 		this.comment.add(comment);
 	}
 
-
-
 	public List<Like> getLike() {
 		return like;
 	}
 
-
-
 	public void addLike(Like like) {
-		this.like= new ArrayList<Like>();
+		this.like = new ArrayList<Like>();
 		this.like.add(like);
 	}
 
@@ -253,11 +183,10 @@ public class User {
 		DataAcess dataaccess = new DataAcess();
 		return dataaccess.login(username, password);
 	}
-	
-	public void register(String firstName, String lastName, 
-			String gender, int birthDate, 
-			String email, String city, String state, String street, int zipcode,String password,String username){
-		DataAcess.registerUser(firstName, lastName, gender, birthDate,
-				email,city,state,street,zipcode,password,username);
+
+	public void register(String firstName, String lastName, String gender, int birthDate, String email, String city,
+			String state, String street, int zipcode, String password, String username) {
+		DataAcess.registerUser(firstName, lastName, gender, birthDate, email, city, state, street, zipcode, password,
+				username);
 	}
 }
